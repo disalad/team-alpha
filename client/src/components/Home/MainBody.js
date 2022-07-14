5ca348348cbeb10acabd89f0c7311e2b4da0f5bb
Disala Damsas <damsasdisala@gmail.com>
2022-07-14 16:56:58 +0530

    Client: Add main body section

--- client/src/components/Home/MainBody.js
+++ client/src/components/Home/MainBody.js
@@ -1,5 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons';
+
 function MainBody() {
-    return <>Main Body</>;
+    return (
+        <section id="home">
+            <div className="text-center">
+                <h1 className="title">Team Alpha</h1>
+            </div>
+            <div className="socials">
+                <article className="telegram social-icon">
+                    <a href="https://t.me/AlphaTm_Botz" className="social-icon-link">
+                        <FontAwesomeIcon icon={faTelegram} className="icon" />
+                    </a>
+                </article>
+                <article className="github social-icon">
+                    <a href="https://github.com/TeamAlphaTg" className="social-icon-link">
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                    </a>
+                </article>
+                <article className="twitter social-icon">
+                    <a href="https://twitter.com" className="social-icon-link">
+                        <FontAwesomeIcon icon={faTwitter} className="icon" />
+                    </a>
+                </article>
+            </div>
+            <a href="#aboutus" role="button" className="more-btn">
+                More About Us
+            </a>
+        </section>
+    );
 }
 
 export default MainBody;

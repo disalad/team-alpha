eac0768a544dd20f4686a98a3d47b0b64222a9e0
Disala Damsas <damsasdisala@gmail.com>
2022-07-14 17:15:13 +0530

    Client: Change SCSS folder structure

--- client/src/components/Home/MainBody.js
+++ client/src/components/Home/MainBody.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import './mainBody.scss';
 
 function MainBody() {
     return (

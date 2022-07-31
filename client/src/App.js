bc98f8ece7c356a94cf293dc694a993f4f0fd6fd
Disala Damsas <damsasdisala@gmail.com>
2022-07-31 20:51:42 +0530

    Client: Add recent projects section

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
+import ProjectsContextProvider from './context/ProjectsContext';
 
 function App() {
     return (
         <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path="/" exact element={<Home />} />
-                </Routes>
-            </Layout>
+            <ProjectsContextProvider>
+                <Layout>
+                    <Routes>
+                        <Route path="/" exact element={<Home />} />
+                    </Routes>
+                </Layout>
+            </ProjectsContextProvider>
         </BrowserRouter>
     );
 }

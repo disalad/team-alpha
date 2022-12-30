8388e659a91afe564dd88780d7179aaf03def415
Disala Damsas <damsasdisala@gmail.com>
2022-12-30 21:18:44 +0530

    Add tabs in technologies section

--- client/src/components/Home/Technologies.js
+++ client/src/components/Home/Technologies.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
 import './technologies.scss';
 
 function Technologies() {
+    const [currentTab, setCurrentTab] = useState('langs');
+
     return (
         <section id="technologies">
             <div className="technologies-container">
-                <h1 className="section-title">Technologies</h1>
+                <div className="p-3rem">
+                    <h1 className="section-title">Technologies</h1>
+
+                    {/* Tabs */}
+                    <ul className="technologies-tab" role="tablist">
+                        <li className="tab-item" role="presentation">
+                            <button
+                                type="button"
+                                role="tab"
+                                id="technologies-tabs-languages-tab"
+                                className={`technology-tab ${
+                                    currentTab === 'langs' ? 'active' : ''
+                                }`}
+                                onClick={() => setCurrentTab('langs')}
+                            >
+                                Languages &amp; Tools
+                            </button>
+                        </li>
+                        <li className="tab-item" role="presentation">
+                            <button
+                                type="button"
+                                role="tab"
+                                id="technologies-tabs-databases-tab"
+                                className={`technology-tab ${
+                                    currentTab === 'dbs' ? 'active' : ''
+                                }`}
+                                onClick={() => setCurrentTab('dbs')}
+                            >
+                                Databases &amp; DevOps
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     );

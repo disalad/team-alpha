7fed3722b032e4e78f31e8711425191eed3df527
Disala Damsas <damsasdisala@gmail.com>
2022-12-31 19:59:19 +0530

    Front end files took out of the client folder

--- server/api/routes/admin.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const AdminsController = require('../controllers/AdminsController');
-
-router.get('/', AdminsController.fetch_data);
-
-module.exports = router;
